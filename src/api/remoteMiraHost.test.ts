898259a3ed46b9f173c6d7daf9ebf53a5110069f
t-zt <407604671@qq.com>
2026-09-16 20:27:27 +0800

    fix: keep Relay transport mock non-generic

--- src/api/remoteMiraHost.test.ts
+++ src/api/remoteMiraHost.test.ts
@@ -1020,9 +1020,9 @@ describe('RemoteMiraHostClient memory surface', () => {
       return request.parse(memoryOverviewPayload);
     };
     let relayCalls = 0;
-    const relayJson: RelayJsonTransport = async () => {
+    const relayJson: RelayJsonTransport = async (_relay, _request) => {
       relayCalls += 1;
-      return memoryOverviewPayload as Awaited<ReturnType<RelayJsonTransport>>;
+      return memoryOverviewPayload;
     };
     const client = new RemoteMiraHostClient(store, direct, undefined, relayJson);
 
